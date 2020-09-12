2bf86ea7273c80d39d8cef9d4c90ace382bb09fe
Maycon Nascimento de Oliveira <mayconnascimentodeoliveira23@gmail.com>
2020-09-12 16:55:53 -0300

    tarefa respostas prontas

--- implementar.php
+++ implementar.php
@@ -1,7 +1,10 @@
 -----Implementar-----
--restringir a ter no minimo nivel 1 
+
+- Restringir a ter no minimo nivel 1 
 para alterar qualquer parametro no banco e 
 nivel 2 para laboratorio;
 
--Verificar um BUG de sessao: Não tenho certeza mas acho que ta encerrando a sessao do nada ou perdendo os dados de $_SESSION['nivel'] ai mesmo estando logado no nivel 1 depois de um tempo não
+- Verificar um BUG de sessao: Não tenho certeza mas acho que ta encerrando a sessao do nada ou perdendo os dados de $_SESSION['nivel'] ai mesmo estando logado no nivel 1 depois de um tempo não
 permite fazer algumas modificacoes!!!
+
+- Ver código do farm de respostas prontas e tentar embutir no labniveleitor
