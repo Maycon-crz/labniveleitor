ca1f2523a82397d91c93f6b012fd1902bbfc456b
Maycon Nascimento de Oliveira <mayconnascimentodeoliveira23@gmail.com>
2020-09-29 22:40:42 -0300

    Organizando tarefas

--- implementar.php
+++ implementar.php
@@ -1,12 +1,44 @@
 -----Implementar-----
 
-- Restringir a ter no minimo nivel 1 
-para alterar qualquer parametro no banco e 
-nivel 2 para laboratorio;
 
-- Verificar um BUG de sessao: Não tenho certeza mas acho que ta encerrando a sessao do nada ou perdendo os dados de $_SESSION['nivel'] ai mesmo estando logado no nivel 1 depois de um tempo não
-permite fazer algumas modificacoes!!!
+--------->Modificações site online:	
+	
 
-- Ver código do farm de respostas prontas e tentar embutir no labniveleitor
+<---------
+--->Tarefas 
+	--->Mais urgentes
 
--Zerar o pedido da filial quando clicar em pronto
+		-> Quando pronto ao invés de pedido pronto na frase, escrever pedidos no balde, pegar a quantidade de itens do pedido e embutir no cadastro do aviso;
+		-> Remover margin left e right dos botões do menu poção para melhorar o layout cortado;
+
+		-> Encontrar uma forma de mostrar as pastas atrasadas de ontêm;
+		-> Adicionar mais estas opções no menu poção labniveleitor: 
+		Anastrozol Diluido; Metilcobalamina diluida;
+		-> Restringir a ter no minimo nivel 1 
+		para alterar qualquer parametro no banco e 
+		nivel 2 para laboratorio;
+		-> Verificar um BUG de sessao: Não tenho certeza mas acho que ta encerrando a sessao do nada ou perdendo os dados de $_SESSION['nivel'] ai mesmo estando logado no nivel 1 depois de um tempo não
+		permite fazer algumas modificacoes!!!(Tempo de sessão está com apenas 180 minutos procurar uma forma de aumentar)
+		
+	<---
+	--->Menos urgentes
+		-> Ver código do farm de respostas prontas e tentar embutir no labniveleitor
+	<---		
+<---
+-----------------------Concluidos(Organizar por DATA!!!)-----------------------
+	
+	--->Homologação
+
+		<--
+			-> Zerar o pedido da filial quando clicar em pronto
+		-->(29/09/2020)
+
+		<--
+
+	<---
+
+	--->online
+
+	<---
+
+-------------------------------------------------------------------------
