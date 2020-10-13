bb51795ef03907aa6b012fe57d4edb3a79438953
Maycon Nascimento de Oliveira <mayconnascimentodeoliveira23@gmail.com>
2020-10-13 20:02:49 -0300

    BT Base Efervescente Abacaxi Incompleto

--- painel.php
+++ painel.php
@@ -1020,6 +1020,13 @@
 						<button type='button' class='btsFaltaProducao form-control btn btn-warning' id='producao'>Produção</button>
 					</div>
 				</li><li>
+					<button type='button' class='btsPreProntos mt-2' id='BaseEfervescenteAbacaxi'>Base Efervescente<br/>Abacaxi</button>
+				</li><li>
+					<div class='linhaPreProntos' id='linhaBaseEfervescenteAbacaxi'>
+						<button type='button' class='btsFaltaProducao form-control btn btn-danger' id='acabou'>Acabando</button>
+						<button type='button' class='btsFaltaProducao form-control btn btn-warning' id='producao'>Produção</button>
+					</div>
+				</li><li>
 					<button type='button' class='btsPreProntos mt-2' id='Almoco'>Almoço</button>
 				</li><li>
 					<div class='linhaPreProntos' id='linhaAlmoco'>
