8e593bd790142ae9b7d04c5816f1c1813080e302
Maycon Nascimento de Oliveira <mayconnascimentodeoliveira23@gmail.com>
2020-11-15 20:03:00 -0300

    Organizando Tarefas

--- implementar.php
+++ implementar.php
@@ -7,32 +7,29 @@
 
 <---------
 --->Tarefas 
-	--->Mais urgentes		
-
-		-> Terminar pre prontos adicionais digitados no input e novas funcoes para mensagens automaticas
-
-		-> (Talvez não precise com a nova Dinamica)Criei botões: Metil testosterona(Para peixe), Base Efervescente Abacaxi, limão e laranja, Vitamina B12 diluida, T3 diluido, T4 Diluido, Vitamina D3 Diluida Porém não implementei eles no
-		Javascript nem na tabela do Banco de Dados!;
-
-		-> Remodelar ao invés de botões criar input para adicionar pré-prontos Digitando o nome mas ainda
-		com as opções Acabando e Acabou;
+	--->Mais urgentes
 
 		-> Restringir a ter no minimo nivel 1 
 		para alterar qualquer parametro no banco e 
-		nivel 2 para coisas do laboratorio;
-		
+		nivel 2 para coisas do laboratorio;		
 
 	<---
 	--->Menos urgentes
-		-> No menu poção fazer botao diluídos para os diluídos ou botao Pré Sólidos e Pré Semi-sólidos	
+		-> No menu poção fazer classificação botao diluídos para os diluídos ou botao Pré Sólidos e Pré Semi-sólidos	
 	<---		
 <---
 -----------------------Concluidos(Organizar por DATA!!!)-----------------------
 	
 	--->Homologação
+		-->(15/11/2020)
+			-> Terminar pre prontos adicionais digitados no input e novas funcoes para mensagens automaticas
+			-> (Talvez não precise com a nova Dinamica)Criei botões: Metil testosterona(Para peixe), Base Efervescente Abacaxi, limão e laranja;
+			->Criado Opção de digitar pre pronto;
+		<--
 		-->(14/11/2020)
-			-> Verificar um BUG de sessao que dura apenas 180 minutos perdendo os dados de $_SESSION['nivel'] ai mesmo estando logado no nivel 1 depois de um tempo não
+			-> Verificado um BUG de sessao que durava apenas 180 minutos  perdendo os dados de $_SESSION['nivel'] ai mesmo estando logado no nivel 1 depois de um tempo não
 			permite fazer algumas modificacoes!!!(Tempo de sessão está com apenas 180 minutos procurar uma forma de aumentar)(Não testei, testar no laboratorio para ver se funciona...);
+			agora durando 12 horas 
 		<--
 		-->(27/10/2020)
 			-> Na hora de Editar a Quantidade de pastas editar a tabela do dia especificado no select!
